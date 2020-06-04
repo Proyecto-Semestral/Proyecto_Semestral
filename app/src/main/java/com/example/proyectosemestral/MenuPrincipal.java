86ea5180694dcb23accdeadf9914919edaee626b
Proyecto-Semetral <maflapproyecto@gmail.com>
2020-06-04 19:56:50 -0400

    se implementa el menu lateral desplegable

--- app/src/main/java/com/example/proyectosemestral/MenuPrincipal.java
+++ app/src/main/java/com/example/proyectosemestral/MenuPrincipal.java
@@ -7,61 +7,26 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MenuPrincipal extends AppCompatActivity {
-    private BottomNavigationView bottomNavigationView;
-    private FrameLayout frameLayout;
 
-    private FragmentGuantes fragmentGuantes;
-    private FragmentMascarillas fragmentMascarillas;
-    private FragmentJabon fragmentJabon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
 
-        fragmentGuantes = new FragmentGuantes();
-        fragmentMascarillas = new FragmentMascarillas();
-        fragmentJabon = new FragmentJabon();
-        bottomNavigationView = findViewById(R.id.botonNavigationView);
-        frameLayout = findViewById(R.id.framelayout);
-
-        setFragment(fragmentGuantes);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.opcionGuantes:
-                        bottomNavigationView.setItemBackgroundResource(R.color.celesteMedio);
-                        setFragment(fragmentGuantes);
-                        return true;
-                    case R.id.opcionMascarilla:
-                        bottomNavigationView.setItemBackgroundResource(R.color.celesteMedio);
-                        setFragment(fragmentMascarillas);
-                        return true;
-                    case R.id.opcionJabon:
-                        bottomNavigationView.setItemBackgroundResource(R.color.celesteMedio);
-                        setFragment(fragmentJabon);
-                        return true;
-
-                }
-                return false;
-            }
-        });
-
-    }
-
-    private void setFragment(Fragment fragment){
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.framelayout,fragment);
-        fragmentTransaction.commit();
     }
 
 
